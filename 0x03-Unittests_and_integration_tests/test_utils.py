467a78a96e283c48a4af2e2956d1ccf4725a8c99
felikz97 <felikz.kipkemoi@gmail.com>
2025-05-28 21:05:16 +0300

    parameterize patch 6

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -39,14 +39,13 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
 
-
-
 class TestMemoize(unittest.TestCase):
-    
+
     def test_memoize(self):
         class TestClass:
             def a_method(self):
                 return 42
+
             @memoize
             def a_property(self):
                 return self.a_method()
