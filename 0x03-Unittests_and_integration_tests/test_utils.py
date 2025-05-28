78f6181a7a4e0e67eeffab1c8b6326b11295fe6b
felikz97 <felikz.kipkemoi@gmail.com>
2025-05-28 21:26:52 +0300

    parameterize patch 16

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """Test that access_nested_map returns the correct value."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
