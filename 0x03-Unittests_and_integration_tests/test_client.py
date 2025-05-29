cf78fb3bb7a706297180fc01395c48d6bba5a5c5
felikz97 <felikz.kipkemoi@gmail.com>
2025-05-29 10:44:42 +0300

    more patching 2

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -20,7 +20,9 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient(org_name)
         result = client.org
 
-        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+            )
         self.assertEqual(result, expected)
 
 
@@ -30,10 +32,14 @@ if __name__ == "__main__":
 
 class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos_url(self):
-        test_payload = {"repos_url": "https://api.github.com/orgs/testorg/repos"}
+        test_payload = (
+            {"repos_url": "https://api.github.com/orgs/testorg/repos"}
+        )
         expected_url = "https://api.github.com/orgs/testorg/repos"
 
-        with patch.object(GithubOrgClient, "org", new_callable=PropertyMock) as mock_org:
+        with patch.object(
+            GithubOrgClient, "org", new_callable=PropertyMock
+        ) as mock_org:
             mock_org.return_value = test_payload
             client = GithubOrgClient("testorg")
             result = client._public_repos_url
