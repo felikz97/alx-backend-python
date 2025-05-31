7b1dbc8cda9464a8b118236748d14c54e822ddf2
felikz97 <felikz.kipkemoi@gmail.com>
2025-05-31 15:57:33 +0300

    APIs endpoints

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
 router = DefaultRouter()
+# Register viewsets with the router
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
