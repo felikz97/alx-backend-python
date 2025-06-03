0602e00ed625c192d723a67e9c8c69a44f109d2c
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-03 08:45:26 +0300

    authentication

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'chats',
     'rest_framework',
     'corsheaders',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'rest_framework.authtoken'
 ]
 
 REST_FRAMEWORK = {
