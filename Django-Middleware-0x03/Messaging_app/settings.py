804d1ca0f1d4e993e69bee39a8ca9200d900717f
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-07 17:02:41 +0300

    1 Logging User

--- Django-Middleware-0x03/Messaging_app/settings.py
+++ Django-Middleware-0x03/Messaging_app/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,7 +112,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    'chats.middleware.RequestLoggingMiddleware'
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
