b236c302110e76b74db00f30e898965b9d65ec87
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-07 17:06:33 +0300

    1 Logging User

--- Django-Middleware-0x03/Messaging_app/settings.py
+++ Django-Middleware-0x03/Messaging_app/settings.py
@@ -15,6 +15,11 @@ from datetime import timedelta
 
 import os
 
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chats.settings')
+
+application = get_wsgi_application()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
