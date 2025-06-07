49bdef00393346e5546d89bee7138b6fbb403fa8
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-07 18:33:34 +0300

    2 Restrict Chat Access by time

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -17,9 +17,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chats.settings')
 
-application = get_wsgi_application()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
