d69998a4a156dff449afc26c3b7d6aa8c2f88ac8
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-07 16:49:49 +0300

    0 project set up

--- Django-Middleware-0x03/Messaging_app/urls.py
+++ Django-Middleware-0x03/Messaging_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mDjango-Middleware-0x03 project.
+URL configuration for messaging_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib  import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework.authtoken import views as auth_views
-from django.conf import settings
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),  # Assuming your app's URLs
