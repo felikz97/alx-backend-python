fce2a7e8f2e3ff4b71c3e4e6c1dbaac6a1e58ce8
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-11 15:00:09 +0300

    Custom ORM Manager for Unread Messages

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -2,9 +2,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UnreadMessagesManager(models.Manager):
-    def for_user(self, user):
-        return self.get_queryset().filter(receiver=user, read=False).only('id', 'content', 'sender', 'timestamp')
+from .managers import UnreadMessagesManager
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
