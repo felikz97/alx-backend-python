e64ab81d18f54d9dc6e5cd24adb4bd578ea08c6f
felikz97 <felikz.kipkemoi@gmail.com>
2025-06-11 13:47:44 +0300

    creating a signal for logging message edit

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -8,6 +8,8 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='edited_messages')
+    edited_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Message from {self.sender} to {self.receiver} at {self.timestamp}"
